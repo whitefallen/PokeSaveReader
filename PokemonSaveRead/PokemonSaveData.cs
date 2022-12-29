5bd40c4d1e00ff88740d75360ca4f90beab272b9
Whitefallen <t.kammann1614@web.de>
2022-12-29 19:28:06 +0100

    Disabled Badge - currently not able to read

--- PokemonSaveRead/PokemonSaveData.cs
+++ PokemonSaveRead/PokemonSaveData.cs
@@ -8,13 +8,13 @@ namespace PokemonSaveRead
     {
         public List<Pokemon> Party { get; set; }
         public string TrainerName { get; set; }
-        public int CurrentBadges { get; set; }
+        //public int CurrentBadges { get; set; }
         public string PlayedTime { get; set; }
         public PokemonSaveData()
         {
             this.Party = new List<Pokemon>();
             this.TrainerName = "SampleTrainerName";
-            this.CurrentBadges = 0;
+            //this.CurrentBadges = 0;
             this.PlayedTime = "00:00:00";
         }
     }
